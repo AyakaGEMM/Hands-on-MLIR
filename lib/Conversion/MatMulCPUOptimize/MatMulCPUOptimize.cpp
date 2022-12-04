33d540a8531ab9945c5bf950270cfc5ff61c8be5
Pzzzzz <hello-cd.plus@hotmail.com>
2022-12-04 22:18:20 +0800

    add MatMulCPUOptimize base

--- lib/Conversion/MatMulCPUOptimize/MatMulCPUOptimize.cpp
+++ lib/Conversion/MatMulCPUOptimize/MatMulCPUOptimize.cpp
@@ -13,16 +13,56 @@
 using namespace mlir;
 
 namespace {
-class MatMulCPUOptimize : public ConversionPattern {
+struct MatMulCPUOptimize : public ConversionPattern {
   MatMulCPUOptimize(MLIRContext *ctx)
       : ConversionPattern(linalg::MatmulOp::getOperationName(), 1, ctx) {}
 
   LogicalResult
   matchAndRewrite(Operation *op, ArrayRef<Value> operands,
                   ConversionPatternRewriter &rewriter) const final {
+    auto loc = op->getLoc();
 
+    // Get input A, B, C.
+    Value A = op->getOperand(0);
+    Value B = op->getOperand(1);
+    Value C = op->getOperand(2);
+    // Get shape of input and output
+    ShapedType ATy = A.getType().cast<ShapedType>();
+
+    // Some constants.
+    const Value i = rewriter.create<arith::ConstantIndexOp>(loc, 0);
+    const Value c0 =
+        rewriter.create<arith::ConstantOp>(loc, rewriter.getIndexAttr(0));
+    const Value c1 =
+        rewriter.create<arith::ConstantOp>(loc, rewriter.getIndexAttr(1));
+    const AffineExpr s0 = rewriter.getAffineSymbolExpr(0);
+    const AffineExpr s1 = rewriter.getAffineSymbolExpr(1);
+    const AffineExpr d0 = rewriter.getAffineDimExpr(0);
+    const AffineExpr d1 = rewriter.getAffineDimExpr(1);
+    const AffineMap mapBroadcast =
+        AffineMap::get(2, 0, rewriter.getAffineConstantExpr(0));
+    const VectorType vTy = VectorType::get(16, ATy.getElementType());
+
+    // Dims
+    Value M = rewriter.create<memref::DimOp>(loc, A, 0);
+    Value N = rewriter.create<memref::DimOp>(loc, B, 1);
+    Value K = rewriter.create<memref::DimOp>(loc, A, 1);
+
+    buildAffineLoopNest(
+        rewriter, loc, {i}, {K}, K_BLOCK_SIZE,
+        [&](OpBuilder &builder, Location loc, ValueRange ivRange) {
+          Value ik = ivRange.front();
+        });
+
+    rewriter.eraseOp(op);
     return success();
   }
+
+  const size_t M_KERNEL_SIZE = 6;
+  const size_t N_KERNEL_SIZE = 16;
+  const size_t K_BLOCK_SIZE = 1024;
+  const size_t M_BLOCK_SIZE = 384;
+  const size_t N_BLOCK_SIZE = 1024;
 };
 } // namespace
 
@@ -47,7 +87,7 @@ struct MatMulCPUOptimizePass
 } // namespace
 
 void MatMulCPUOptimizePass::runOnOperation() {
-
+  MLIRContext *context = &getContext();
   ConversionTarget target(getContext());
 
   // We define the specific operations, or dialects, that are legal targets for
@@ -55,6 +95,14 @@ void MatMulCPUOptimizePass::runOnOperation() {
   // `Affine`, `Arith`, `Func`, and `MemRef` dialects.
   target.addLegalDialect<AffineDialect, BuiltinDialect, arith::ArithDialect,
                          func::FuncDialect, memref::MemRefDialect>();
+  target.addIllegalOp<linalg::MatmulOp>();
+
+  RewritePatternSet patterns(context);
+  patterns.add<MatMulCPUOptimize>(context);
+
+  if (failed(
+          applyPartialConversion(getOperation(), target, std::move(patterns))))
+    signalPassFailure();
 }
 
 namespace mlir {
