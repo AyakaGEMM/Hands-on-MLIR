Fix mask error, Fix attention precision error, Update status

examples/torch/bert_attention/bert_attention.py
@@ -1,13 +1,15 @@
 import torch
 import torch_mlir
-from transformers import BertConfig
+from transformers import BertConfig, BertForMaskedLM
 from transformers.models.bert.modeling_bert import BertAttention
 
 torch.manual_seed(42)
 
+hs = 768
+
 encoded_input_list = [
-    torch.rand((1, 64, 128)),
-    torch.zeros((1, 64), dtype=torch.int32),
+    torch.rand((1, 64, hs)),
+    torch.ones((1, 64), dtype=torch.int32),
 ]
 
 
@@ -15,20 +17,18 @@ class BertAttentionWrapper(torch.nn.Module):
     def __init__(self):
         super().__init__()
         config = BertConfig().from_pretrained("bert-base-uncased")
-        config.num_hidden_layers = 2
-        config.hidden_size = 128
-        config.num_attention_heads = 2
         self.attn = BertAttention(config)
         self.attn.self.query.bias.data.zero_()
         self.attn.self.key.bias.data.zero_()
         self.attn.self.value.bias.data.zero_()
         self.attn.output.dense.bias.data.zero_()
+        self.model = BertForMaskedLM(config)
 
     def forward(self, hidden, mask):
         mask = mask[:, None, None, :]
         mask = mask.to(torch.float32)
         mask = (1.0 - mask) * torch.finfo(torch.float32).min
-        return self.attn(hidden, mask)[0]
+        return self.model.bert.encoder(hidden, mask).last_hidden_state
 
 
 model = BertAttentionWrapper()

examples/torch/bert_attention/bert_self_attention.py
@@ -7,7 +7,7 @@ torch.manual_seed(42)
 
 encoded_input_list = [
     torch.rand((1, 64, 128)),
-    torch.zeros((1, 64), dtype=torch.int32),
+    torch.ones((1, 64), dtype=torch.int32),
 ]
 
 
@@ -29,7 +29,7 @@ class BertAttentionWrapper(torch.nn.Module):
         mask = mask[:, None, None, :]
         mask = mask.to(torch.float32)
         mask = (1.0 - mask) * torch.finfo(torch.float32).min
-        return self.ln(self.linear(self.attn(hidden, mask)[0]) + hidden)
+        return self.attn(hidden, mask)[0]
 
 
 model = BertAttentionWrapper()